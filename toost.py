c2c0d2b5981a008d464410ecd6aad3c003361673
Ali <am_alizadeh74@yahoo.com>
2024-01-09 20:57:55 +0100

    all files added according to Prof files

--- toost.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-Np=5#Number of rows - number of patients
-Nf=4#Number of columns - number of features
-A=np.random.randn(Np,Nf) #Gauss. randommatrixA, shape (Np,Nf)
-w=np.random.randn(Nf) #Gauss. randomvector wid, shape (Nf,)
-y=A@w # shape (Np,)
